4d4ce884e05f1868626ca9600659bc3646ad3d93
deshmukh-ajinkya <deshmukhasb@gmail.com>
2025-07-01 09:25:05 +0530

    deployment setup

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/ad-portfolio/", // 🔥 This is crucial
+  base: "/ad-portfolio/",
   plugins: [react()],
 });
